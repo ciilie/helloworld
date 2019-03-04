2d61a13a19aba72ddfeb3e6ad50329e820f2258f
Iulian Cordus <iulian.cordus@sbab.se>
2019-03-04 15:56:43 +0100

    Returning json tasks

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-from flask import Flask
-from redis import Redis, RedisError
+from flask import Flask, jsonify
 import os
 import socket
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+# initial tasks	
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+def index():
+    html = "<h3>Hello world!</h3>" \
+    return html
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)

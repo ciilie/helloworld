904c2d86de9cb35057def2a93777d334c84c795f
Iulian Cordus <iulian.cordus@sbab.se>
2019-03-04 16:00:35 +0100

    calling getClient with 'http://localhosti/todo/api/v1.0/tasks'

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ def getClient(address):
    if resp.status_code != 200:
       # This means something went wrong.
       raise Exception('GET localhost {}'.format(resp))
-   print(resp)
+   print resp
 
 
 if __name__=='__main__':
-   getClient('http://localhost')
+   getClient('http://localhosti/todo/api/v1.0/tasks')

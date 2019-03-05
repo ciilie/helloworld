3d64df8c28fdb1d90eb3c9dc83831152da63008e
Iulian Cordus <iulian.cordus@sbab.se>
2019-03-05 13:38:23 +0100

    TEST: Adding a simple unit test

---
+++ test_client.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_testing import TestCase
+
+TESTING = True
+
+class MyTest(TestCase):
+   # executed prior to each test
+   def setUp(self):
+      app.config['TESTING'] = True
+      app.config['WTF_CSRF_ENABLED'] = False
+      app.config['DEBUG'] = False
+      self.app = app.test_client()
+      self.assertEqual(app.debug, False)
+
+   # executed after each test
+   def tearDown(self):
+      pass
+   
+   def test_getitems(self):
+         response = self.app.get('/', follow_redirects=True)
+         self.assertEqual(response.status_code, 200)
+

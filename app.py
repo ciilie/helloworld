9d16cd415ffe1961adcdda3be1b1ee9fa1a3fd5d
Iulian Cordus <iulian.cordus@sbab.se>
2019-03-05 12:34:11 +0100

    TEST: added addtask route, poptask route

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, request, abort, request, render_template, url_for, flash, redirect, session, url_for, jsonify
+from forms import AddTaskForm
 import os
 import socket
+SECRET_KEY = os.urandom(32)
+
 
 # initial tasks	
 
@@ -21,28 +24,53 @@ tasks = [
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route("/")
 def index():
-    html = "<h3>Hello world!</h3>" \
-    return html
+    output = """<h3>Simple application that works with a list of dictionaries called tasks.<br/></h3>
+             <p>Usage:</p>
+             <li>http://localhost/gettasks -- gets all the current tasks</li/><br/>
+             <li>http://localhost/addtask -- adds a new task to the current tasks</li><br/>          
+             <li>http://localhost/pop -- pop a task from the current tasks</li><br/>
+             """
+    return output
+
 
 @app.route('/gettasks', methods=['GET'])
-def get_tasks(self):
+def get_tasks():
     return jsonify({'tasks': tasks})
 
 
-@app.route('/addtask', methods=['GET'])
-def add_task(self,title,description,done):
-    max_id=max(tasks, key=lambda x:x['id'])
-    newtask={'id':max_id+1,
-             'title': title,
-             'description': description,
-             'done': done
-             }
-    tasks.append(newtask)
-    return jsonify({'tasks': tasks})
+@app.route('/addtask', methods=['GET','POST'])
+def add_task():
+    form=AddTaskForm()
+    if request.method=="POST":
+        if form.validate()==False:
+            return render_template("addtask.html", form=form)
+        else:
+            max_id = max(d['id'] for d in tasks)
+            max_id = int(max_id) + 1
+            newtask = {'id': str(max_id),
+                       'title': str(form.title.data),
+                       'description': str(form.description.data),
+                       'done': str(form.done.data)
+                       }
+            tasks.append(newtask)
+            return jsonify({'tasks': tasks})
+    elif request.method=="GET":
+        return render_template("addtask.html", title="Add new task", form=form)
 
+
+
+
+@app.route('/poptask', methods=['DELETE'])
+def pop_task():
+    if len(tasks)>0:
+        tasks.pop(-1)
+        return jsonify({'tasks': tasks})
+    else:
+        return "No more tasks to pop"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)

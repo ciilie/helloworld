30678b180a80ac08a08263a7597dcf3362814e3e
Iulian Cordus <iulian.cordus@sbab.se>
2019-03-05 14:17:57 +0100

    TEST: Minor fix added the tests directory and calling the tests with python -m unittest discover

---
+++ tests/test_client.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_testing import TestCase
+
+TESTING = True
+
+class MyTest(TestCase):
+   # executed prior to each test
+   def setUp(self):
+      app.config['TESTING'] = True
+      app.config['WTF_CSRF_ENABLED'] = False
+      app.config['DEBUG'] = False
+      self.app = app.test_client()
+      self.assertEqual(app.debug, False)
+
+   # executed after each test
+   def tearDown(self):
+      pass
+
+   def test_returncode(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+   def test_gettasks(self):
+       response = self.app.get('/gettasks', follow_redirects=True)
+       self.assertContains(response, "Buy groceries")
